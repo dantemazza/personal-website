5aa0f427d94bf4ee96f239a8fe4228f90b51e0ed
Dante Mazza <47069410+dantemazza@users.noreply.github.com>
2022-05-19 18:29:08 -0400

    Update About.js

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -11,11 +11,11 @@ function About(){
             <div className= "aboutText">
                 <b><font size="20">Thanks for stopping by!</font></b>
                 <p>
-                    I'm a 3B student in Computer Engineering at the University of Waterloo. I was originally in Chemical Engineering but transferred after becoming interested in software and hardware.        
+                    I'm a fourth year Computer Engineering student at the University of Waterloo. I was originally in Chemical Engineering but transferred after becoming interested in software and hardware.        
                 </p>
                 <p>
                     School aside, I love learning technical skills through side projects.
-                    Outside of tech, I enjoy weightlifting, cooking, running, playing basketball and&nbsp; 
+                    Outside of tech, I enjoy playing basketball, weightlifting, cooking, running, and&nbsp; 
                     <a href="https://www.naqt.com/stats/player/index.jsp?contact_id=416423" className="employerLinks" target="_blank" rel="noopener noreferrer">competitive trivia</a>
                     &nbsp;in my leisure time.
                 </p>
